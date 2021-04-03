e9b844744fd8e84b855f55bc535382464972ee3b
kcastil3 <31296611+kcastil3@users.noreply.github.com>
2021-04-03 15:01:34 -0500

    Update settings.py

--- MenuFolder/MenuService/settings.py
+++ MenuFolder/MenuService/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's9^-rf$@!(4^j)eihd^4lhny#3sic*$qkk$cxno^lis-haav%)'
+SECRET_KEY = 'Omitted'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
